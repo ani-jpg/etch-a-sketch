f4451ccc03c87bc75389284248fdaf066b9bc49a
Annie <113863477+anniet22@users.noreply.github.com>
2023-02-22 18:32:42 -0400

    Created hover function in js

--- eas.js
+++ eas.js
@@ -5,9 +5,17 @@ function createGrid() {
         const square=document.createElement('div');
         square.classList.add("square");
         gridContainer.appendChild(square);
+
+        square.addEventListener ('mouseover', hoverRed) 
     }
+
+    
 }
 
 for (let i=0; i<16; i++) {
     createGrid();
+}
+
+function hoverRed(event){
+    event.target.style.background='red'
 }
