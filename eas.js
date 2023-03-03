9800d357d1ad304638b0f031d56a3446c96b802c
Annie <113863477+anniet22@users.noreply.github.com>
2023-03-03 13:31:45 -0400

    Created grid function that intakes any size, but can't figure out why there's a gap between squares

--- eas.js
+++ eas.js
@@ -1,21 +1,27 @@
-const gridContainer=document.querySelector('.grid-container')
+// prompt function to get the number of grid squares
+let size= prompt ("Give me a size.")
+
+// function to create a grid 
+const grid= document.createElement('div');
+grid.classList.add('grid-container');
+document.body.appendChild(grid);
+grid.style.gridTemplateColumns=`repeat(${size}, 1fr)`;
+grid.style.gridTemplateRows=`repeat(${size}, 1fr)`;
 
 function createGrid() {
-    for (let i=0; i<16; i++) {
-        const square=document.createElement('div');
-        square.classList.add("square");
-        gridContainer.appendChild(square);
-
-        square.addEventListener ('mouseover', hoverRed) 
+    for (i=0; i<size*size; i++) {
+    const square=document.createElement('div');
+    square.classList.add('square');
+    grid.appendChild(square);
+    if (size<=100) {
+        square.style.width=`${470/size}px`;
+        square.style.height=`${470/size}px`;
     }
-
-    
+    }
 }
 
-for (let i=0; i<16; i++) {
-    createGrid();
-}
+createGrid(size);
+console.log(size);
 
-function hoverRed(event){
-    event.target.style.background='red'
-}
+// function for each mode of hover effect 
+
