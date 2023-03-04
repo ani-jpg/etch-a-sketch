4aba470d757b0868a77c030ad21a40fa8209d782
Annie <113863477+anniet22@users.noreply.github.com>
2023-03-04 08:56:05 -0400

    Made choice buttons

--- eas.js
+++ eas.js
@@ -1,10 +1,14 @@
 // prompt function to get the number of grid squares
 let size= prompt ("Give me a size.")
 
+// buttons for each mode of hover effect 
+
+
 // function to create a grid 
+const middleContainer=document.querySelector('.middle-container');
 const grid= document.createElement('div');
 grid.classList.add('grid-container');
-document.body.appendChild(grid);
+middleContainer.appendChild(grid);
 grid.style.gridTemplateColumns=`repeat(${size}, 1fr)`;
 grid.style.gridTemplateRows=`repeat(${size}, 1fr)`;
 
@@ -17,11 +21,13 @@ function createGrid() {
         square.style.width=`${510/size}px`;
         square.style.height=`${510/size}px`;
     }
+
+    square.addEventListener('mouseover',()=> {
+        square.style.backgroundColor='red';
+
+    })
     }
 }
 
 createGrid(size);
 console.log(size);
-
-// function for each mode of hover effect 
-
