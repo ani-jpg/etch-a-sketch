9511b54ba595dcf83af09976f3a210207abc5558
Annie <113863477+anniet22@users.noreply.github.com>
2023-03-12 19:33:54 -0300

    Done everything except the greyscale function, I literally can't figure it out so I'm taking a break from this for a bit

--- eas.js
+++ eas.js
@@ -28,6 +28,14 @@ rainbowChoice.addEventListener('click', () => {
     eraserClick=false;
 })
 
+const greyscaleChoice=document.querySelector('#greyscale')
+let greyscale;
+greyscaleChoice.addEventListener('click', () => {
+    greyscale=true;
+    rainbow=false;
+    color=false;
+    eraserClick=false;
+})
 
 // function to create the interactive grid 
 const middleContainer=document.querySelector('.middle-container');
@@ -37,8 +45,8 @@ middleContainer.appendChild(grid);
 grid.style.gridTemplateColumns=`repeat(${size}, 1fr)`;
 grid.style.gridTemplateRows=`repeat(${size}, 1fr)`;
 
-
 let hover=true;
+let j=1;
 
 function createGrid() {
     for (i=0; i<size*size; i++) {
@@ -49,7 +57,9 @@ function createGrid() {
         square.style.width=`${510/size}px`;
         square.style.height=`${510/size}px`;
     }
-    
+
+    const squares=document.querySelectorAll('.square')
+
     function hoverFunction () {
         if (eraserClick==true) {
             square.style.backgroundColor='';
@@ -61,6 +71,12 @@ function createGrid() {
 
         else if (rainbow==true) {
             square.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        }
+
+        else if (greyscale==true) {
+            const greyColors= ["#EEEEEE", "#CCCCCC", "#999999", "#666666","#333333", "#000000"]
+            square.style.backgroundColor=greyColors[0]; 
+            })
         }
 
         else {
@@ -84,21 +100,16 @@ function createGrid() {
             })
         }
     })
-
-
-    
-
-    reset.addEventListener ('click', ()=> {
-        square.style.backgroundColor='';
-        rainbow=false;
-        eraserClick=false;
-        color=false;
-    })
-
-    
     }
 }
 
+reset.addEventListener ('click', ()=> {
+    grid.querySelectorAll('.square').forEach(square => {
+        square.style.backgroundColor = '';
+    });
+    rainbow=false;
+    eraserClick=false;
+})
 
 createGrid(size);
 console.log(size);
